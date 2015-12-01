64931f07644c7fd3bdb440b3866852722f17914f
irfanphy <m.irfanphy@gmail.com>
2015-12-01 15:58:59 +0100

    testing branch stuff

--- activate_filters.sh
+++ activate_filters.sh
@@ -1,3 +1,3 @@
 git config core.attributesfile .gitattributes
 git config filter.dropoutput_ipynb.clean "python ipynb_output_filter.py"
-git config filter.dropoutput_ipynb.smudge cat
+git config filter.dropoutput_ipynb.smudge "python cachedoutput.py"
